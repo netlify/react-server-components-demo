e2815de2752e79568c693fce11055ea274942c34
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2021-01-01 16:53:12 -0800

    Update build commands

--- webpack.functions.config.js
+++ webpack.functions.config.js
@@ -14,7 +14,6 @@ module.exports = {
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
-    // conditionNames: ["react-server", "..."],
     alias: {
         'pg-native': path.join(__dirname, 'aliases/pg-native.js'),
         'pgpass$': path.join(__dirname, 'aliases/pgpass.js'),
